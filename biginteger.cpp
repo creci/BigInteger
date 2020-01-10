8add5cf98bbc186178a5d15575051c187afc4066
creci <creci@mail.ru>
2020-01-10 23:01:44 +0200

    Fixed bug with record 0. Added prototypes of operators.

--- biginteger.cpp
+++ biginteger.cpp
@@ -4,15 +4,55 @@
 using namespace std;
 BigInteger::BigInteger()
 {
-
+    BigInteger::NullNumber();
+    this->Sign=0;
 }
 void BigInteger::NullNumber(){
-    for(unsigned int i=0;i<4096;i++)
+    for(unsigned int i=0;i<this->size;i++)
         this->Number[i]=0;
+}
+unsigned int BigInteger::SizeNumber(){
+    unsigned int i=this->size-1;
+    for(;this->Number[i]!=1&&i!=0;i--);
+    return i+1;
+}
+void BigInteger::PrintBinary(){
+    if(BigInteger::SizeNumber()==1 && !(this->Number[0])){
+        cout<<"0.0"<<endl;
+        return;
+    }
+    cout<<this->Sign<<".";
+    unsigned int i=this->size-1;
+    for(;!(this->Number[i]);i--);
+    for(;i!=0;i--){
+        cout<<this->Number[i];
+    }
+    cout<<this->Number[i];
+    cout<<endl;
+}
+const BigInteger operator+ (const BigInteger& left,const BigInteger& right){
+    bool p=0;
+    bool buffer=0;
+    BigInteger result;
+    for(unsigned int i=0;i<result.size-1;i++){
+        //cout<<left.Number[i]<<"^"<<right.Number[i]<<endl;
+        buffer=left.Number[i]^right.Number[i];
+        result.Number[i]=buffer^p;
+        p=(p&buffer)|(left.Number[i]&right.Number[i]);
+        cout<<left.Number[i]<<"+"<<right.Number[i]<<"="<<result.Number[i]<<endl;
+    }
+    buffer=left.Sign^right.Sign;
+    result.Sign=buffer^p;
+    p=(p&buffer)|(left.Sign&right.Sign);
+    cout<<"Знак:";
+    cout<<left.Sign<<"+"<<right.Sign<<"="<<result.Sign<<endl;
+    result.PrintBinary();
+    return result;
 }
 BigInteger::BigInteger(string number)
 {
     BigInteger::NullNumber();
+
     if(number.length()==0){
         this->Sign=false;
     }else{
@@ -29,7 +69,7 @@ BigInteger::BigInteger(string number)
         while(temp1.length()!=0 && !((temp1[0]=='1' || temp1[0]=='0')&& temp1.length()==1)){
         while(temp1.length()!=i){
             buffer=buffer*10+temp1[i]-'0';
-            temp2+=std::to_string(buffer/2);
+            temp2+=to_string(buffer/2);
             buffer=buffer%2;
             i++;
         }
@@ -41,7 +81,7 @@ BigInteger::BigInteger(string number)
         temp2.clear();
         buffer=0;
         }
-        this->Number[count++]=1;
+        if(temp1[count]!='0')this->Number[count++]=1;
     }
 }
 
--- biginteger.h
+++ biginteger.h
@@ -5,13 +5,38 @@
 
 class BigInteger
 {
-private:
-    bool Number[4096];
-    bool Sign;
 public:
+    static const int size=4096;
+    bool Number[size];
+    bool Sign;
     BigInteger();
     BigInteger(std::string number);
     void NullNumber();
+    unsigned int SizeNumber();
+    void PrintBinary();
+
+    //Unary +
+    friend const BigInteger& operator+(const BigInteger& i);
+    //Unary -
+    friend const BigInteger operator-(const BigInteger& i);
+    //Prefix increment
+    friend const BigInteger& operator++(const BigInteger& i);
+    //Postfix increment
+    friend const BigInteger operator++(const BigInteger& i,int);
+    //Prefix decrement
+    friend const BigInteger& operator--(const BigInteger& i);
+    //Postfix decrement
+    friend const BigInteger operator--(const BigInteger& i,int);
+
+    //Binary +
+    friend const BigInteger operator+ (const BigInteger& left,const BigInteger& right);
+    //Binary -
+    friend const BigInteger operator- (const BigInteger& left,const BigInteger& right);
+    //Binary *
+    friend const BigInteger operator* (const BigInteger& left,const BigInteger& right);
+    //Binary /
+    friend const BigInteger operator/ (const BigInteger& left,const BigInteger& right);
+
 };
 
 #endif // BIGINTEGER_H
