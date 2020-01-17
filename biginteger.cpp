e2b3de1b1012b77a80e69711051f7e6f2d35de38
creci <creci@mail.ru>
2020-01-17 14:29:05 +0200

    Added operators: - ">>"; - "<<"; - ">>="; - "<<="; - "+="; - "*="; - "-"(Binary); - "bool()".

--- biginteger.cpp
+++ biginteger.cpp
@@ -2,15 +2,18 @@
 #include <stack>
 #include <iostream>
 using namespace std;
+
 BigInteger::BigInteger()
 {
     BigInteger::NullNumber();
     this->Sign=0;
 }
+
 void BigInteger::NullNumber(){
     for(unsigned int i=0;i<this->size;i++)
         this->Number[i]=0;
 }
+
 void BigInteger::Reverse(){
     for(unsigned int i=0;i<this->size;i++)
         this->Number[i]=!(this->Number[i]);
@@ -29,17 +32,19 @@ BigInteger& BigInteger::operator=(const BigInteger& right){
 void BigInteger::Increment(){
     bool p=1;
     bool buffer=0;
-    for(unsigned int i=0;i<this->size-1 && p;i++){
+    for(unsigned int i=0;i<this->size && p;i++){
         buffer=this->Number[i]^0;
         this->Number[i]=buffer^p;
         p=(p&buffer)|(this->Number[i]&0);
     }
 }
+
 unsigned int BigInteger::SizeNumber(){
     unsigned int i=this->size-1;
     for(;this->Number[i]!=1&&i!=0;i--);
     return i+1;
 }
+
 void BigInteger::PrintBinary(){
     if(BigInteger::SizeNumber()==1 && !(this->Number[0])){
         cout<<"0.0"<<endl;
@@ -54,6 +59,22 @@ void BigInteger::PrintBinary(){
     cout<<this->Number[i];
     cout<<endl;
 }
+
+BigInteger::BigInteger(const BigInteger& number){
+    for(unsigned int i=0;i<this->size;i++){
+        this->Number[i]=number.Number[i];
+    }
+    this->Sign=number.Sign;
+}
+
+BigInteger::operator bool()const{
+    if(this->Sign)return 1;
+    for(unsigned int i=0;i<this->size;i++){
+        if(this->Number[i]) return 1;
+    }
+    return 0;
+}
+
 //Binary +
 const BigInteger operator+ (const BigInteger& left,const BigInteger& right){
     bool p=0;
@@ -69,38 +90,116 @@ const BigInteger operator+ (const BigInteger& left,const BigInteger& right){
    // p=(p&buffer)|(left.Sign&right.Sign);
     return result;
 }
+
+//Binary -
+const BigInteger operator- (const BigInteger& left,const BigInteger& right){
+    BigInteger result=left+(-right);
+    return  result;
+}
+
+//Prefix decrement
+const BigInteger& operator--( BigInteger& number){
+    number--;
+    return number;
+}
+
+//Postfix decrement
+const BigInteger operator--( BigInteger& number,int){
+    bool p=0;
+    bool buffer=0,buffer1=0;
+    BigInteger result=number;
+    for(unsigned int i=0;i<result.size;i++){
+        buffer=number.Number[i]^1;
+        buffer1=buffer^p;
+        p=(p&buffer)|(number.Number[i]&1);
+        number.Number[i]=buffer1;
+    }
+    buffer=number.Sign^1;
+    number.Sign=buffer^p;
+    return result;
+}
+
 //Prefix increment
 const BigInteger& operator++( BigInteger& number){
     number++;
     return number;
 }
+
 //Postfix increment
 const BigInteger operator++( BigInteger& number,int){
     bool p=1;
-    bool buffer=0;
+    bool buffer=0,buffer1=0;
     BigInteger result=number;
-    for(unsigned int i=0;i<result.size-1 && p;i++){
+    for(unsigned int i=0;i<result.size && p;i++){
         buffer=number.Number[i]^0;
-        number.Number[i]=buffer^p;
+        buffer1=buffer^p;
         p=(p&buffer)|(number.Number[i]&0);
+        number.Number[i]=buffer1;
     }
     buffer=number.Sign^0;
     number.Sign=buffer^p;
    // p=(p&buffer)|(number.Sign&0);
     return result;
 }
+
 //Unary +
 const BigInteger& operator+(const BigInteger& number){
     return number;
 }
+
 //Unary -
 const BigInteger operator-(const BigInteger& number){
     BigInteger result=number;
-    result.Sign=!(result.Sign);
-    result.Reverse();
-    result.Increment();
+    if(result.SizeNumber()!=1 || result.Number[0]){
+        result.Sign=!(result.Sign);
+        result.Reverse();
+        result++;
+    }
     return  result;
 }
+
+//Binary << | +
+BigInteger operator<<(const BigInteger& number, int n){
+    BigInteger Result=number;
+    return Result<<=n;
+}
+
+BigInteger& operator<<= ( BigInteger& left,unsigned int n){
+    if(n==0)return left;
+    BigInteger Result=left;
+    for(;n!=0;n--){
+        for(unsigned int i=1;i<left.size;i++){
+            Result.Number[i]=left.Number[i-1];
+        }
+        Result.Number[0]=left.Sign;
+    }
+    left=Result;
+    return left;
+}
+
+//Binary >> | +
+BigInteger operator>>(const BigInteger& number, int n){
+    BigInteger Result=number;
+    return Result>>=n;
+
+}
+
+//Binary =>> | +
+BigInteger& operator>>= ( BigInteger& left,unsigned int n){
+    if(n==0)return left;
+    BigInteger Result=left;
+    for(;n!=0;n--){
+        for(unsigned int i=left.size-1;i!=0;){
+            i--;
+            Result.Number[i]=left.Number[i+1];
+        }
+        Result.Number[Result.size-1]=left.Sign;
+    }
+    left=Result;
+    return left;
+
+}
+
 BigInteger::BigInteger(const string number)
 {
     string temp1(number);
@@ -121,20 +220,20 @@ BigInteger::BigInteger(const string number)
         stack<bool> TempNumber;
         int buffer=0;
         unsigned int i=0,count=0;
-        while(temp1.length()!=0 && !((temp1[0]=='1' || temp1[0]=='0')&& temp1.length()==1)){
-        while(temp1.length()!=i){
-            buffer=buffer*10+temp1[i]-'0';
-            temp2+=to_string(buffer/2);
-            buffer=buffer%2;
-            i++;
-        }
-        i=0;
-        this->Number[count++]=buffer;
-        while(temp2[0]=='0')
-            temp2=temp2.substr(1);
-        temp1=temp2;
-        temp2.clear();
-        buffer=0;
+        while(temp1.length()!=0 && ((temp1[0]!='1' && temp1[0]!='0')|| temp1.length()!=1)){
+            while(temp1.length()!=i){
+                buffer=buffer*10+temp1[i]-'0';
+                temp2+=to_string(buffer/2);
+                buffer=buffer%2;
+                i++;
+            }
+            i=0;
+            this->Number[count++]=buffer;
+            while(temp2[0]=='0')
+                temp2=temp2.substr(1);
+            temp1=temp2;
+            temp2.clear();
+            buffer=0;
         }
         if(temp1[count]!='0')this->Number[count++]=1;
         if(this->Sign ){
@@ -144,3 +243,47 @@ BigInteger::BigInteger(const string number)
     }
 }
 
+BigInteger& operator*=(BigInteger& left,const BigInteger& right){
+    if(!(left)){
+        return left;
+    }else if(!(right)){
+        return left=right;
+    }else{
+        bool sign=left.Sign^right.Sign;
+        if(left.SizeNumber()==1){
+            left=right;
+            left.Sign=sign;
+            return left;
+        }
+        BigInteger buffer=right;
+        if(buffer.SizeNumber()==1){
+            left.Sign=sign;
+            return left;
+        }
+        if(left.Sign){
+            left--;
+            left.Reverse();
+        }
+        if(buffer.Sign){
+            buffer--;
+            buffer.Reverse();
+        }
+        BigInteger temp;
+        left.Sign=0;
+        for(int i=0;i<=buffer.SizeNumber();i++,left<<=1){
+            if(buffer.Number[i]){
+                temp+=left;
+            }
+        }
+        if(sign){
+            temp.Reverse();
+            temp.Increment();
+        }
+        left=temp;
+        left.Sign=sign;
+        return left;
+    }
+}
+BigInteger& operator+=( BigInteger& left, const BigInteger& right){
+    return left=left+right;
+}
--- biginteger.h
+++ biginteger.h
@@ -6,34 +6,50 @@
 class BigInteger
 {
 public:
-    static const int size=4096;
-    bool Number[size];
-    bool Sign;
+    static const int size=4098;
     BigInteger();
+    BigInteger(const BigInteger& number);
     BigInteger(const std::string number);
     void NullNumber();
     unsigned int SizeNumber();
     void PrintBinary();
     void Reverse();
     void Increment();
-
+protected:
+    bool Number[size];
+    bool Sign;
+public:
+    //Assignment operator | +
     BigInteger& operator= (const BigInteger& right);
-    //Unary +
+    //Unary + | +
     friend const BigInteger& operator+(const BigInteger& number);
-    //Unary -
+    //Unary - | +
     friend const BigInteger operator-(const BigInteger& number);
-    //Prefix increment
+    //Prefix increment | +
     friend const BigInteger& operator++( BigInteger& number);
-    //Postfix increment
+    //Postfix increment | +
     friend const BigInteger operator++( BigInteger& number,int);
-    //Prefix decrement
+    //Prefix decrement | +
     friend const BigInteger& operator--( BigInteger& number);
-    //Postfix decrement
+    //Postfix decrement | +
     friend const BigInteger operator--( BigInteger& number,int);
+    operator bool()const;//Bool |+
 
-    //Binary +
+    //Binary >> | +
+    friend BigInteger operator>>(const BigInteger& number, int n);
+    //Binary << | +
+    friend BigInteger operator<<(const BigInteger& number, int n);
+    //Binary >>= | +
+    friend BigInteger& operator>>= ( BigInteger& left,unsigned int n);
+    //Binary <<= | +
+    friend BigInteger& operator<<= ( BigInteger& left,unsigned int n);
+    //Binary +=
+    friend BigInteger& operator+=( BigInteger& left, const BigInteger& right);
+    //Binary *=
+    friend BigInteger& operator*=(BigInteger& left,const BigInteger& right);
+    //Binary + | +
     friend const BigInteger operator+ (const BigInteger& left,const BigInteger& right);
-    //Binary -
+    //Binary - | +
     friend const BigInteger operator- (const BigInteger& left,const BigInteger& right);
     //Binary *
     friend const BigInteger operator* (const BigInteger& left,const BigInteger& right);
